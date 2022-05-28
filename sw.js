8cb954c5494b222fd15828ab3d414fe1e216487c
Sebastian Schmidt <publicarray@users.noreply.github.com>
2022-05-29 09:43:42 +1000

    add manifest

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
-const cacheName = 'password-generator-v1';
-const files = ["/", "/index.html", "/eff.txt"]
+// https://jakearchibald.com/2014/offline-cookbook/
+// https://web.dev/offline-cookbook/
+const cacheName = 'password-generator-v1.1';
+const files = ["/", "/index.html", "/manifest.json", "/eff.txt"]
 
 self.addEventListener("install", (e) => {
   e.waitUntil((async () => {
@@ -12,14 +14,8 @@ self.addEventListener("install", (e) => {
 
 self.addEventListener('fetch', (e) => {
     e.respondWith((async () => {
-        const r = await caches.match(e.request)
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-        if (r) { return r }
-        const response = await fetch(e.request)
-        const cache = await caches.open(cacheName)
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone())
-        return response
+        let response = await caches.match(e.request)
+        return response || await fetch(e.request)
     })())
 })
 
