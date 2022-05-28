60847b1386f0cd8b19b6abc882bd913cae8f25ab
Sebastian Schmidt <publicarray@users.noreply.github.com>
2022-05-29 09:05:15 +1000

    service worker

---
+++ sw.js
@@ -0,0 +1,33 @@
+// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+const cacheName = 'password-generator-v1';
+const files = ["/", "/index.html", "/eff.txt"]
+
+self.addEventListener("install", (e) => {
+  e.waitUntil((async () => {
+        const cache = await caches.open(cacheName)
+        await cache.addAll(files)
+    })())
+})
+
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+        const r = await caches.match(e.request)
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) { return r }
+        const response = await fetch(e.request)
+        const cache = await caches.open(cacheName)
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone())
+        return response
+    })())
+})
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key === cacheName) { return }
+            return caches.delete(key)
+        }))
+    }))
+})
